179cb30ad66f4093f7ccaecba8d05d9a88ae0324
Mohammed Algadhib <algadhib@Mohammeds-MacBook-Pro.local>
2023-01-20 14:10:54 +0300

    end of chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::startup::run;
+use std::net::TcpListener;
+use zero2prod::configurations::get_configurations;
+use sqlx::PgPool;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configurations = get_configurations().expect("Failed to read configurations.");
+
+    let connection_pool = PgPool::connect(
+            &configurations.database.connection_string()
+        )
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let address = format!("127.0.0.1:{}", configurations.application_port);
+    let listener = TcpListener::bind(address)
+        .expect("Failed to bind random port.");
+    run(listener, connection_pool)?.await
 }
